2fd4743e5a2e31e988fc922a97115542ac89a956
Rick <heinrichchan@gmail.com>
2017-07-06 00:35:40 +0800

    added env config

--- config/index.js
+++ config/index.js
@@ -1,8 +1,22 @@
+/*eslint no-process-env: "off"*/
+let host, port;
+switch (process.env.NODE_ENV) {
+	case 'docker': {
+		host = "redis",
+		port = 6379
+		break;
+	}
+	default: {
+		host = "192.168.99.100",
+		port = 6379
+		break;
+	}
+}
 module.exports = {
 	app: {
 		redis: {
-			host: "192.168.99.100",
-			port: 6379
+			host: host,
+			port: port
 		},
 		apiKey: 'AIzaSyAThTaWYcdO7M9-sn74ikhrmfwl-ZbCYAQ'
 	}
